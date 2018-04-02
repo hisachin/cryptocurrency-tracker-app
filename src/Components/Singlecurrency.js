7e34cb5fcb8adf64bb2ebfe3725264884f2490d3
sachin <sachinj@247around.com>
2018-04-03 01:05:09 +0530

    change colot

--- src/Components/Singlecurrency.js
+++ src/Components/Singlecurrency.js
@@ -10,9 +10,9 @@ class Singlecurrency extends Component {
                 <div className="single-currency-div" key = {id}>
                     <p className="cryptocurrency-name">{name} ({symbol})</p>
                           <h1><i className="fas fa-rupee-sign"></i>{ (+price_inr).toFixed(2) }</h1>
-                          <p> <span className = {percent_change_1h >= 0 ? 'green' : 'red'}>{percent_change_1h}% </span> 1hr</p>
-                          <p> <span className = {percent_change_1h >= 0 ? 'green' : 'red'}>{percent_change_24h}% </span> 24hrs</p>
-                          <p> <span className = {percent_change_1h >= 0 ? 'green' : 'red'}>{percent_change_7d}% </span>7days</p>
+                          <p> <span className = {percent_change_1h < 0 ? 'red' : 'green'}>{percent_change_1h}% </span> in past 1hr</p>
+                          <p> <span className = {percent_change_24h < 0 ? 'red' : 'green'}>{percent_change_24h}% </span> in past 24hrs</p>
+                          <p> <span className = {percent_change_7d < 0 ? 'red' : 'green'}>{percent_change_7d}% </span> in past 7days</p>
                 </div>
               </div>
     );
